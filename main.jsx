24d00cf4193d816fac839a9b2ac94edeb9f83e7b
rahulrakesh10 <rahul.rakesh1012@gmail.com>
2025-06-30 22:32:49 -0400

    changes

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ProjectCarousel from './ProjectCarousel'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('react-projects')).render(
+  <React.StrictMode>
+    <ProjectCarousel />
+  </React.StrictMode>
+)
